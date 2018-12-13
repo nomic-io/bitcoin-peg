11bd66ae1e04a1db5723b19798d5f760389827f3
Matt Bell <mappum@gmail.com>
2018-12-12 21:52:21 -0800

    Support proving multiple deposit txs at once

--- src/index.js
+++ src/index.js
@@ -53,67 +53,66 @@ module.exports = function (initialHeader, coinName) {
   }
 
   function depositTx (state, tx, context) {
-    // TODO: support proving multiple txs
-
-    // decode transaction
-    let txBytes = tx.transaction
-    let bitcoinTx = protocol.types.transaction.decode(txBytes)
-
-    // verify tx format
-    // TODO: use a format that supports joining deposits for multiple people
-    if (tx.outs.length !== 2) {
-      throw Error('Deposit tx should have exactly 2 outputs')
-    }
-    // verify first output pays to signatory set
-    // TODO: compare against older validator sets
-    let expectedP2ss = deposit.createOutput(context.validators, state.signatorySet)
-    let depositOutput = tx.outs[0]
-    if (!depositOutput.script.equals(expectedP2ss)) {
-      throw Error('Invalid deposit output')
-    }
-    // verify second output commits to recipient address
-    let commitmentOutput = bitcoin.payments.embed(tx.outs[1])
-    if (commitmentOutput.data.length !== 20) {
-      throw Error('Invalid recipient address commitment output')
-    }
-
-    // get hash of tx
-    let txid = getTxHash(bitcoinTx)
-    let txidBase64 = txid.toString('base64')
-
-    // verify tx is confirmed deep enough
-    // TODO
-    // TODO: use heuristic based on value
-
-    // verify tx hasn't already been processed
-    if (state.processedTxs[txidBase64]) {
-      throw Error('Deposit transaction has already been processed')
-    }
-    state.processedTxs[txidBase64] = true
-
     // get specified block header from state
     let chain = Blockchain({ store: state.chain })
-    let header = chain.getByHeight(tx.proof.height)
+    let header = chain.getByHeight(tx.height)
 
     // verify proof is connected to block, and is valid
     tx.proof.merkleRoot = header.merkleRoot
     let txids = verifyMerkleProof(tx.proof)
 
-    // verify tx hash is included in proof
-    if (txids.length !== 1) {
-      throw Error('Expected exactly one txid included in proof')
-    }
-    if (!txids[0].equals(txid)) {
-      throw Error('Merkle proof does not match given transaction')
-    }
+    // process each transaction
+    let i = 0
+    for (let txBytes of tx.transactions) {
+      // decode transaction
+      let bitcoinTx = protocol.types.transaction.decode(txBytes)
 
-    // mint satoshis for recipient address
-    let addressHash = commitmentOutput.data
-    context.modules[coinName].mint({
-      address: coins.hashToAddress(addressHash),
-      amount: depositOutput.value
-    })
-    console.log('minting ' + depositOutput.value + ' for ' + coins.hashToAddress(addressHash))
+      // get hash of tx
+      let txid = getTxHash(bitcoinTx)
+      let txidBase64 = txid.toString('base64')
+
+      // verify tx hasn't already been processed
+      if (state.processedTxs[txidBase64]) {
+        throw Error('Deposit transaction has already been processed')
+      }
+      state.processedTxs[txidBase64] = true
+
+      // verify tx hash is included in proof
+      if (!txids[i].equals(txid)) {
+        throw Error('Transaction does not match proof')
+      }
+      i += 1
+
+      // verify tx format
+      // TODO: use a format that supports joining deposits for multiple people
+      if (bitcoinTx.outs.length !== 2) {
+        throw Error('Deposit tx should have exactly 2 outputs')
+      }
+      // verify first output pays to signatory set
+      // TODO: compare against older validator sets
+      let expectedP2ss = deposit.createOutput(context.validators, state.signatorySet)
+      let depositOutput = bitcoinTx.outs[0]
+      if (!depositOutput.script.equals(expectedP2ss)) {
+        throw Error('Invalid deposit output')
+      }
+      // verify second output commits to recipient address
+      let commitmentOutput = bitcoin.payments.embed(bitcoinTx.outs[1])
+      if (commitmentOutput.data.length !== 20) {
+        throw Error('Invalid recipient address commitment output')
+      }
+
+      // verify tx is confirmed deep enough
+      // TODO
+      // TODO: use heuristic based on value
+
+      // mint satoshis for recipient address
+      let addressHash = commitmentOutput.data
+      context.modules[coinName].mint({
+        address: coins.hashToAddress(addressHash),
+        amount: depositOutput.value
+      })
+      console.log('minting ' + depositOutput.value + ' for ' + coins.hashToAddress(addressHash))
+    }
   }
 
   function signatoryKeyTx (state, tx, context) {
