991991bfa8dc4128bc2305266ac48e7545c4b357
Matt Bell <mappum@gmail.com>
2019-01-25 12:57:27 -0800

    Added signatory signing and disbursal relaying to integration test

--- test/integration.js
+++ test/integration.js
@@ -22,6 +22,8 @@ params.defaultPort = 18444
 params.magic = 0xdab5bffa
 
 test('integration (bitcoind + lotion app + relayers)', async (t) => {
+  // TODO: use multiple validators
+
   let dataPath = join(tmpdir(), Math.random().toString(36))
   let bitcoinPath = join(dataPath, 'bitcoin')
   mkdirSync(dataPath)
@@ -32,7 +34,8 @@ test('integration (bitcoind + lotion app + relayers)', async (t) => {
     regtest: true,
     datadir: bitcoinPath,
     debug: 1,
-    deprecatedrpc: 'signrawtransaction'
+    deprecatedrpc: 'signrawtransaction',
+    txindex: 1
   })
   await bitcoind.started()
   console.log('started bitcoind')
@@ -153,6 +156,50 @@ test('integration (bitcoind + lotion app + relayers)', async (t) => {
     }
   })
   console.log('relayed deposit')
+
+  await wallet.send({
+    type: 'bitcoin',
+    amount: 1e8,
+    script: Buffer.from([ 1, 2, 3, 4 ])
+  })
+  deepEqual(t, await client.state.bitcoin.utxos, [])
+  deepEqual(t, await client.state.bitcoin.withdrawals, []) // (already processed and put into "signingTx")
+  t.is(await client.state.bitcoin.signedTx, null)
+  deepEqual(t, await client.state.bitcoin.signingTx, {
+    inputs: [{
+      amount: 9999990000,
+      index: 0,
+      txid: depositTxid
+    }],
+    outputs: [{
+      amount: 1e8,
+      script: Buffer.from([ 1, 2, 3, 4 ])
+    }],
+    signatures: [],
+    signedVotingPower: 0
+  })
+  console.log('sent withdrawal transaction')
+
+  await peg.signatory.signDisbursal(client, signatoryPriv)
+  let signedTx = await client.state.bitcoin.signedTx
+  let disbursalTx = peg.relay.buildDisbursalTransaction(
+    signedTx,
+    peg.relay.convertValidatorsToLotion(client.validators),
+    signatoryKeyState
+  )
+  t.is(await client.state.bitcoin.signingTx, null)
+  t.is(signedTx.signatures.length, 1)
+  t.is(signedTx.signedVotingPower, 10)
+  deepEqual(t, await client.state.bitcoin.utxos, [{
+    amount: 9899990000,
+    index: 1,
+    txid: disbursalTx.getHash()
+  }])
+  console.log('signed disbursal transaction')
+
+  await bitcoind.rpc.sendRawTransaction(disbursalTx.toHex())
+  t.deepEqual(await bitcoind.rpc.getRawMempool(), [ disbursalTx.getId() ])
+  console.log('relayed disbursal transaction')
 
   // cleanup
   bitcoind.kill()
