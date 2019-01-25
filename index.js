4cce6d34dc39419b855b9977d2a115ed587e4c6a
Matt Bell <mappum@gmail.com>
2019-01-25 11:45:34 -0800

    Added deposit transaction creation helper

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ module.exports = require('./src/index.js')
 module.exports.createDepositOutput = require('./src/reserve.js').createOutput
 module.exports.relay = require('./src/relay.js')
 module.exports.signatory = require('./src/signatory.js')
+module.exports.deposit = require('./src/deposit.js')
