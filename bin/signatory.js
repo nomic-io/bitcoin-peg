103e74c0419ae50fde256cfe34e0e210890e6cff
Matt Bell <mappum@gmail.com>
2019-01-31 17:36:54 -0800

    Combined signatory commitment and signing into one script

--- bin/signatory.js
+++ bin/signatory.js
@@ -1,10 +1,18 @@
+#!/usr/bin/env node
+
 'use strict'
 
-let { readFileSync } = require('fs')
+let { readFileSync, writeFileSync } = require('fs')
 let { randomBytes, createHash } = require('crypto')
+let { join, dirname } = require('path')
 let secp = require('secp256k1')
 let ed = require('ed25519-supercop')
 let { connect } = require('lotion')
+let {
+  commitPubkey,
+  signDisbursal,
+} = require('../src/signatory.js')
+let DJSON = require('deterministic-json')
 
 async function main () {
   let gci = process.argv[2]
@@ -15,55 +23,57 @@ async function main () {
     process.exit(1)
   }
 
+  // load privValidator
+  let privValidatorJSON = readFileSync(privValidatorPath)
+  let privValidator = JSON.parse(privValidatorJSON)
+
+  // load or generate signatory key
+  let signatoryKey
+  let signatoryKeyPath = join(dirname(privValidatorPath), 'priv_signatory.json')
+  try {
+    let signatoryKeyJSON = readFileSync(signatoryKeyPath)
+    signatoryKey = DJSON.parse(signatoryKeyJSON)
+    console.log('loaded signatory private key')
+  } catch (err) {
+    signatoryKey = { priv: randomBytes(32) }
+    let signatoryKeyJSON = DJSON.stringify(signatoryKey)
+    writeFileSync(signatoryKeyPath, signatoryKeyJSON)
+    console.log(`generated signatory private key, saved to "${signatoryKeyPath}"`)
+  }
+
   let client = await connect(gci)
   console.log('connected to peg zone network')
 
-  // let privKey
-  // do {
-  //   privKey = randomBytes(32)
-  // } while (!secp.privateKeyVerify(privKey))
-  let privKey = Buffer.from('LBPpa6mU5J4JK/A2LP5hrrXDxOJv3d+gCOROwQyWjNo=', 'base64')
-  console.log('generated signatory privkey:', privKey.toString('base64'))
-
-  // pubkey
-  let signatoryKey = secp.publicKeyCreate(privKey)
-
-  let validatorJSON = readFileSync(privValidatorPath, 'utf8')
-  let validator = JSON.parse(validatorJSON)
-  let validatorPriv = Buffer.from(validator.priv_key.value, 'base64')
-  let validatorPub = Buffer.from(validator.pub_key.value, 'base64')
-  let convertedPriv = convertEd25519(validatorPriv)
-
-  let signature = ed.sign(signatoryKey, validatorPub, convertedPriv)
-
-  // TODO: locate our validator key in validators array
-  let signatoryIndex = 0
-
-  let tx = {
-    type: 'bitcoin',
-    signatoryIndex,
-    signatoryKey,
-    signature
+  // ensure we haven't committed to a key yet
+  let signatoryPub = secp.publicKeyCreate(signatoryKey.priv)
+  let committedPub = await client.state.bitcoin.signatoryKeys[privValidator.pub_key.value]
+  if (committedPub != null) {
+    if (!committedPub.equals(signatoryPub)) {
+      console.log('already committed to a different signatory key. i hope you didn\'t lose the private key you committed to...')
+      process.exit(1)
+    }
+  } else {
+    // commit to a signatory key
+    await commitPubkey(client, privValidator, signatoryPub)
+    console.log('committed to signatory key on chain')
   }
 
-  let res = await client.send(tx)
-
-  console.log('sent tx', res)
-
-  process.exit(0)
-}
-
-function sha512 (data) {
-  return createHash('sha512').update(data).digest()
-}
-
-function convertEd25519 (ref10Priv) {
-  // see https://github.com/orlp/ed25519/issues/10#issuecomment-242761092
-  let privConverted = sha512(ref10Priv.slice(0, 32))
-  privConverted[0] &= 248
-  privConverted[31] &= 63
-  privConverted[31] |= 64
-  return privConverted
+  // sign transactions as needed
+  while (true) {
+    try {
+      await signDisbursal(client, signatoryKey.priv)
+      console.log('signed disbursal')
+    } catch (err) {
+      if (err.message !== 'No tx to be signed') {
+        throw err
+      }
+    }
+    await delay(5000)
+  }
 }
 
 main().catch(function (err) { throw err })
+
+function delay (ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
