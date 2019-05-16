2335ebd4bcd4d601af802e692c8c196e806d40ce
Judd <kepzorz@gmail.com>
2019-05-16 14:04:24 -0700

    add wallaby config

---
+++ wallaby.js
@@ -0,0 +1,11 @@
+module.exports = function(wallaby) {
+  return {
+    env: {
+      type: 'node',
+      runner: 'node'
+    },
+    files: ['src/*.ts'],
+    tests: ['test/*.ts'],
+    testFramework: 'ava'
+  }
+}
