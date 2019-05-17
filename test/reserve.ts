b049d6c67964c469f8a7d254d4ef429246e29b39
Judd <kepzorz@gmail.com>
2019-05-16 17:29:02 -0700

    add witness script creation snapshot test

--- test/reserve.ts
+++ test/reserve.ts
@@ -1,8 +1,25 @@
 import test from 'ava'
-import { getVotingPowerThreshold } from '../src/reserve'
+import { getVotingPowerThreshold, createWitnessScript } from '../src/reserve'
+import * as seed from 'random-bytes-seed'
+let randomBytes = seed('seed')
+
+const validatorMap = {
+  'aOSx00CgYJ3/WGNgioJEs91irUHNvy+bV20hRTby7ak=': 10,
+  'bbbx00CgYJ3/WGNgioJEs91irUHNvy+bV20hRTby7ak=': 10
+}
+
+const signatoryMap = {
+  'aOSx00CgYJ3/WGNgioJEs91irUHNvy+bV20hRTby7ak=': randomBytes(32),
+  'bbbx00CgYJ3/WGNgioJEs91irUHNvy+bV20hRTby7ak=': randomBytes(32)
+}
 
 test('voting power threshold calculation is 2/3 of total', function(t) {
   let signatorySet = [{ votingPower: 30 }, { votingPower: 30 }]
   let votingPowerThreshold = getVotingPowerThreshold(signatorySet)
   t.is(votingPowerThreshold, 40)
+})
+
+test('witness script creation', function(t) {
+  let witnessScript = createWitnessScript(validatorMap, signatoryMap)
+  t.snapshot(witnessScript)
 })
