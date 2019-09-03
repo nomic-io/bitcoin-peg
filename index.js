67c11252c9ee1d8c6fd3419519dc706650f97e63
Judd <kepzorz@gmail.com>
2019-09-03 15:24:25 -0700

    update test script config

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict'
-
 module.exports = require('./src/index.js')
 module.exports.createDepositOutput = require('./src/reserve.js').createOutput
 module.exports.relay = require('./src/relay.js')
